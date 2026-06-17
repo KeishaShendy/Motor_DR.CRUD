73dbe169bc497739177fc86930d6406a8fcc7197
keishashendy <shendyarrafli7@gmail.com>
2026-06-17 22:56:26 +0700

    tambah tampilan form rekap mahasiswa (label, input, tombol, dan tabel

---
+++ CRUDMahasiswaADO/Form3.Designer.cs
@@ -0,0 +1,31 @@
+﻿namespace CRUDMahasiswaADO
+{
+    partial class frmRekapMahasiswa_Load
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblJudul = new System.Windows.Forms.Label();
+            this.lblProdi = new System.Windows.Forms.Label();
+            this.lblTahunMasuk = new System.Windows.Forms.Label();
+            this.dtpTahunMasuk = new System.Windows.Forms.DateTimePicker();
+            this.cmbProdi = new System.Windows.Forms.ComboBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnCetak = new System.Windows.Forms.Button();
+            this.dgvMahasiswa = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMahasiswa)).BeginInit();
+            this.SuspendLayout();
+            // 
+           
