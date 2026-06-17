5c77dcf4af6ad85f7be84e8ba432d7b5f5e2922e
keishashendy <shendyarrafli7@gmail.com>
2026-06-17 22:56:51 +0700

    atur layout form rekap mahasiswa dan event handler tombol

--- CRUDMahasiswaADO/Form3.Designer.cs
+++ CRUDMahasiswaADO/Form3.Designer.cs
@@ -28,4 +28,111 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgvMahasiswa)).BeginInit();
             this.SuspendLayout();
             // 
-           
+            // lblJudul
+            // 
+            this.lblJudul.AutoSize = true;
+            this.lblJudul.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJudul.Location = new System.Drawing.Point(270, 20);
+            this.lblJudul.Name = "lblJudul";
+            this.lblJudul.Size = new System.Drawing.Size(248, 20);
+            this.lblJudul.TabIndex = 0;
+            this.lblJudul.Text = "REKAP DATA MAHASISWA";
+            // 
+            // lblProdi
+            // 
+            this.lblProdi.AutoSize = true;
+            this.lblProdi.Location = new System.Drawing.Point(73, 70);
+            this.lblProdi.Name = "lblProdi";
+            this.lblProdi.Size = new System.Drawing.Size(39, 16);
+            this.lblProdi.TabIndex = 1;
+            this.lblProdi.Text = "Prodi";
+            // 
+            // lblTahunMasuk
+            // 
+            this.lblTahunMasuk.AutoSize = true;
+            this.lblTahunMasuk.Location = new System.Drawing.Point(364, 70);
+            this.lblTahunMasuk.Name = "lblTahunMasuk";
+            this.lblTahunMasuk.Size = new System.Drawing.Size(88, 16);
+            this.lblTahunMasuk.TabIndex = 2;
+            this.lblTahunMasuk.Text = "Tahun Masuk";
+            // 
+            // dtpTahunMasuk
+            // 
+            this.dtpTahunMasuk.Location = new System.Drawing.Point(458, 65);
+            this.dtpTahunMasuk.Name = "dtpTahunMasuk";
+            this.dtpTahunMasuk.Size = new System.Drawing.Size(150, 22);
+            this.dtpTahunMasuk.TabIndex = 3;
+            // 
+            // cmbProdi
+            // 
+            this.cmbProdi.FormattingEnabled = true;
+            this.cmbProdi.Location = new System.Drawing.Point(118, 65);
+            this.cmbProdi.Name = "cmbProdi";
+            this.cmbProdi.Size = new System.Drawing.Size(220, 24);
+            this.cmbProdi.TabIndex = 4;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(630, 64);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(100, 25);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnCetak
+            // 
+            this.btnCetak.Location = new System.Drawing.Point(630, 390);
+            this.btnCetak.Name = "btnCetak";
+            this.btnCetak.Size = new System.Drawing.Size(100, 30);
+            this.btnCetak.TabIndex = 6;
+            this.btnCetak.Text = "Cetak";
+            this.btnCetak.UseVisualStyleBackColor = true;
+            this.btnCetak.Click += new System.EventHandler(this.btnCetak_Click);
+            // 
+            // dgvMahasiswa
+            // 
+            this.dgvMahasiswa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMahasiswa.Location = new System.Drawing.Point(76, 110);
+            this.dgvMahasiswa.Name = "dgvMahasiswa";
+            this.dgvMahasiswa.RowHeadersWidth = 51;
+            this.dgvMahasiswa.RowTemplate.Height = 24;
+            this.dgvMahasiswa.Size = new System.Drawing.Size(654, 260);
+            this.dgvMahasiswa.TabIndex = 7;
+            // 
+            // frmRekapMahasiswa_Load
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvMahasiswa);
+            this.Controls.Add(this.btnCetak);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.cmbProdi);
+            this.Controls.Add(this.dtpTahunMasuk);
+            this.Controls.Add(this.lblTahunMasuk);
+            this.Controls.Add(this.lblProdi);
+            this.Controls.Add(this.lblJudul);
+            this.Name = "frmRekapMahasiswa_Load";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Filter & Rekap Data";
+            this.Load += new System.EventHandler(this.frmRekapMahasiswa_Load_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMahasiswa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblJudul;
+        private System.Windows.Forms.Label lblProdi;
+        private System.Windows.Forms.Label lblTahunMasuk;
+        private System.Windows.Forms.DateTimePicker dtpTahunMasuk;
+        private System.Windows.Forms.ComboBox cmbProdi;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnCetak;
+        private System.Windows.Forms.DataGridView dgvMahasiswa;
+    }
+}
